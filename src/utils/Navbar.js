ce57e49ea348987ac7ca23261854107df6b9f660
YSW2 <jrgyber@gmail.com>
2024-07-05 03:00:36 +0900

    init commit

---
+++ src/utils/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Button, IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton size="large" edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div">
+          YSW
+        </Typography>
+        <Button color="inherit" component={Link} to="/">
+          Home
+        </Button>
+        <Button color="inherit" component={Link} to="/about">
+          About
+        </Button>
+        <Button color="inherit" component={Link} to="/contact">
+          Contact
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
---
+++ src/utils/theme.js
@@ -0,0 +1,22 @@
+// src/utils/theme.js
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2", // 파란색
+    },
+    secondary: {
+      main: "#dc004e", // 분홍색
+    },
+  },
+  typography: {
+    fontFamily: "Roboto, Arial, sans-serif",
+    h1: {
+      fontSize: "2.5rem",
+      fontWeight: 500,
+    },
+  },
+});
+
+export default theme;
